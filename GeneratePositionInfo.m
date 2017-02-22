4418bc52180b77ef49e2f7d2c518e6eefea0c23c
Shuyu233 <lsyatpku@gmail.com>
2017-02-22 12:08:18 -0500

    Add files via upload

---
+++ GeneratePositionInfo.m
@@ -0,0 +1,20 @@
+[N_2,tmp]=size(X);
+Nc=sqrt(N_2)
+R_all=zeros(Nc^2*(Nc^2-1)/2,1);
+Count=0;
+for i=1:(Nc^2-1)
+    R_all((Count+1):(Count+Nc^2-i))=sqrt((X(i)-X((i+1):Nc^2)).^2+(Y(i)-Y((i+1):Nc^2)).^2);
+    Count=Count+Nc^2-i;
+end
+R_all_sort=sort(R_all);
+Count=0;
+R_set=[];
+Map=[];
+while Count<Nc^2*(Nc^2-1)/2
+    R_new=R_all_sort(Count+1);
+    R_set=[R_set;R_new];
+    p=find(R_all==R_new);
+    [Np tmp]=size(p);
+    Count=Count+Np;
+    Map=[Map;{p}];
+end
